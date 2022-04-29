211aeee2452ee3f3bfd09c0895a60425d9d5771b
Zhen She <neal.seah@gmail.com>
2022-04-30 01:54:28 +0800

    refactor: rename 01&02

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,8 +1,8 @@
 const BASE = Cypress.env('base')
 
 describe('HTML', () => {
-  it('01_basic.html', () => {
-    cy.visit(BASE + '01_basic.html')
+  it('01_head.html', () => {
+    cy.visit(BASE + '01_head.html')
 
     cy.get('html > head > meta')
       .should('have.attr', 'charset', 'utf-8')
@@ -12,8 +12,8 @@ describe('HTML', () => {
       .should('contain', 'This is my first html page.')
   })
 
-  it('02_basic_body.html', () => {
-    cy.visit(BASE + '02_basic_body.html')
+  it('02_structure.html', () => {
+    cy.visit(BASE + '02_structure.html')
 
     cy.get('body > header')
       .should('contain', 'This is the header of this website')
