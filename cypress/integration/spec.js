2b826e3e8be1d2328414b63e80c47c00ebac1508
Zhen She <neal.seah@gmail.com>
2022-05-02 01:49:43 +0800

    feat(08_form_with_types_of_fields)

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -81,4 +81,27 @@ describe('HTML', () => {
       cy.get('input[type="reset"][value="clear all"]')
     })
   })
+
+  it('08_form_with_types_of_fields.html', () => {
+    cy.visit(BASE + '08_form_with_types_of_fields.html')
+
+    cy.get('body > form').within(() => {
+      cy.get('label[for="fullname"]+input#fullname[type="text"][placeholder]')
+      cy.get('label[for="password"]+input#password[type="password"][minlength="8"]')
+      cy.get('label[for="keyword"]+input#keyword[type="search"]')
+      cy.get('label[for="blog"]+input#blog[type="url"]')
+      cy.get('label[for="phone"]+input#phone[type="tel"]')
+      cy.get('label[for="email"]+input#email[type="email"]')
+      cy.get('label[for="age"]+input#age[type="number"]')
+      cy.get('label[for="bio"]+textarea#bio')
+      cy.get('p#season input[type="radio"][name="season"]').should('have.length', 4)
+      cy.get('p#season input[type="radio"][name="season"]').eq(0).should('have.attr', 'checked')
+      cy.get('p#lang input[type="checkbox"][name="language"]').should('have.length', 3)
+      cy.get('p#lang input[type="checkbox"][name="language"]').eq(1).should('have.attr', 'checked')
+      cy.get('label[for="fruit"]+select#fruit[name="fruit"] > option').should('have.length', 3)
+      cy.get('label[for="fruit"]+select#fruit[name="fruit"] > option').last().should('have.attr', 'selected')
+      cy.get('label[for="brightness"]+input#brightness[type="range"][min="0"][max="100"]')
+      cy.get('label[for="birthday"]+input#birthday[type="date"]')
+    })
+  })
 })
